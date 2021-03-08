82776a9fe6d8fe963bb61de52da95c6097c9fe90
makashovivan <makashovivan@gmail.com>
2021-03-08 18:57:46 +0300

    common styles

---
+++ lib/common/styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+BorderRadius getRadius() => BorderRadius.all(Radius.circular(20.0));
+
+Gradient getGradient() => LinearGradient(
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+    colors: [Colors.indigo[700], Colors.pink[300]]);
+
+Padding wrapScreen({Widget screen}) => Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: screen,
+    );
