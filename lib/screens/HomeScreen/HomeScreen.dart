ec9555bf5ace7c9cabaac0ded3eb9fe104281f4b
makashovivan <makashovivan@gmail.com>
2021-03-09 23:00:07 +0300

    total balance banner

--- lib/screens/HomeScreen/HomeScreen.dart
+++ lib/screens/HomeScreen/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:financial_app/screens/HomeScreen/Balance/TotalBalance.dart';
 import 'package:financial_app/screens/HomeScreen/Costs/Costs.dart';
 import 'package:financial_app/screens/HomeScreen/Incomes/Incomes.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Expanded(flex: 8, child: Placeholder()),
+        Expanded(flex: 8, child: TotalBalance()),
         Expanded(flex: 14, child: Incomes()),
         Spacer(),
         Expanded(flex: 14, child: Costs()),
